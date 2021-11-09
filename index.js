f38df508b7e9ddfc6d6bbb2fbc6566644500be36
dHinagas <hinagasrodriguez@gmail.com>
2021-11-09 10:41:40 -0400

    Add sentry for handler errors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 require('./mongo')
 
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
@@ -22,6 +24,28 @@ app.set('port', process.env.PORT || PORT)
 // Middleware
 app.use(cors())
 app.use(morgan('dev'))
+
+Sentry.init({
+  dsn: 'https://b7f724e6263a4019805fadb1010da3a4@o1065088.ingest.sentry.io/6056400',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app })
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+
 app.use(express.json())
 app.use(express.static('build'))
 
@@ -34,6 +58,8 @@ app.get('/info', (req, res) => {
 })
 
 app.use(notFound)
+
+app.use(Sentry.Handlers.errorHandler())
 app.use(handleErrors)
 
 app.listen(app.get('port'), () => console.log(`server listening on port ${app.get('port')}`))
